13a42cd612ff2c6b3ca4f4fd3a4b7413de0edb56
Hideaki SAWADA <chuno@live.jp>
2013-11-27 13:55:05 +0100

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['menu']                  = '自動ログオフ設定';
+$lang['loggedoff']             = '%s  は自動ログオフしました。';
+$lang['usergroup']             = 'ユーザー／グループ';
+$lang['time']                  = '時間（分）';
+$lang['save']                  = '追加';
+$lang['mintime']               = '2 分以上か、自動ログオフが無効になる 0 でなければならない。指定時間は調整されました。';
+$lang['remove']                = '削除';
+$lang['js']['title']           = '自動ログオフ';
+$lang['js']['warn']            = '一定時間休止しました。一分後に自動ログオフします。';
+$lang['js']['stillhere']       = 'いいえ、ログオフしません！';
