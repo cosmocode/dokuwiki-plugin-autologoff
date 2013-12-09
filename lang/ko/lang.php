5d2bdf73791710b1aa6b799ede0fa57d39296883
Myeongjin <aranet100@gmail.com>
2013-12-09 05:20:05 +0100

    translation update

---
+++ lang/ko/lang.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['menu']                  = '자동 로그-오프 설정';
+$lang['loggedoff']             = '%s님이 자동으로 로그 오프되었습니다';
+$lang['usergroup']             = '사용자/그룹';
+$lang['time']                  = '시간 (분)';
+$lang['save']                  = '항목 추가';
+$lang['mintime']               = '시간은 적어도 2분으로 주거나 자동로그아웃을 비활성하려면 0으로 주세요. 지정된 시간이 조정되었습니다.';
+$lang['remove']                = '제거';
+$lang['js']['title']           = '자동 로그-오프';
+$lang['js']['warn']            = '잠시 동안 비활성되었습니다. 몇 분 정도에 자동으로 로그 오프됩니다.';
+$lang['js']['stillhere']       = '아니에요, 아직 여기 있어요!';
