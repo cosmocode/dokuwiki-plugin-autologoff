9ec206e05a320e72f7fab5eb971cc316187e1fb9
Marina Vladi <deldadam@gmail.com>
2014-04-28 00:35:33 +0200

    translation update

---
+++ lang/hu/lang.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Marina Vladi <deldadam@gmail.com>
+ */
+$lang['menu']                  = 'Automatikus kijelentkeztetés beállítása';
+$lang['loggedoff']             = '%s automatikusan kijelentkeztetve.';
+$lang['usergroup']             = 'Felhasználó/Csoport';
+$lang['time']                  = 'Idő (perc)';
+$lang['save']                  = 'Bejegyzés hozzáadása';
+$lang['mintime']               = 'Az időnek legalább 2 percnek kell lennie, vagy 0-át is megadhatunk az automatikus kijelentkeztetés letiltásához. A megadott időt módosítottam.';
+$lang['remove']                = 'törlés';
+$lang['js']['title']           = 'Automatikus kijelentkeztetés';
+$lang['js']['warn']            = 'Már egy ideje inaktív vagy. Körülbelül 1 perc múlva automatikusan kijelentkeztetünk.';
+$lang['js']['stillhere']       = 'Ne! Még itt vagyok.';
