f24b4947fcbae06c6c0580f48f785b411edc9a5a
Gregory Loichot <gregory.loichot@unige.ch>
2024-04-15 18:15:57 +0200

    Update helper.php

--- helper.php
+++ helper.php
@@ -39,7 +39,7 @@ class helper_plugin_autologoff extends DokuWiki_Plugin {
      */
     public function add_entry($usergroup, $time) {
         $time = (int) $time;
-        if ($usergroup == '' || $time == '') {
+        if ($usergroup == '') {
             msg($this->getLang('groupCannotBeEmpty'), -1);
             return;
         }
