4ba232186fec4528c1a750a0dafee08d1ad1099d
Gregory Loichot <gregory.loichot@unige.ch>
2024-04-15 18:12:11 +0200

    Update FR lang.php

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -12,6 +12,7 @@ $lang['usergroup']             = 'Utilisateur/Groupe';
 $lang['time']                  = 'Temps (minutes)';
 $lang['save']                  = 'Ajoutez une entrée';
 $lang['mintime']               = 'Le temps doit être d\'au moins 2 minutes. Ou 0 pour désactiver la déconnexion automatique. Le temps précisé a été ajusté.';
+$lang['groupCannotBeEmpty']    = 'L\'utilisateur/groupe et/ou le temps ne peuvent pas être vides.';
 $lang['remove']                = 'supprimer';
 $lang['js']['title']           = 'Déconnexion automatique';
 $lang['js']['warn']            = 'Vous avez été inactif pendant un certain temps. Vous serez automatiquement déconnecté d\'ici une minute environ.';
